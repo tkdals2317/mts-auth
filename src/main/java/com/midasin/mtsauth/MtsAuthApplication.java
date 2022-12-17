b8782f2aec36be3a0853c21a15f05bdfb9403362
lsm0506 <lsm0506@midasin.com>
2022-12-17 14:58:48 +0900

    Initial commit

---
+++ src/main/java/com/midasin/mtsauth/MtsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.midasin.mtsauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midasin/mtsauth/MtsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midasin.mtsauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
